896941dc08cd651bf7430e9fd35df82d3ccd1997
Hasse Augustinussen <hasse_augu@hotmail.dk>
2020-12-14 00:02:53 +0100

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("specflow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("specflow")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d19c5c5-ee33-4081-b6ba-87f0ce5c1578")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpecFlowFeatureExamSteps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace specflow
+{
+    /// <summary>
+    /// to run the tests Comment out "ScenarioContext.StepIsPending();"
+    /// </summary>
+    [Binding]
+    public class SpecFlowFeatureExamSteps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.StepIsPending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.StepIsPending();
+        }
+
+        [Then(@"I Am a Happy camper")]
+        public void ThenIAmAHappyCamper()
+        {
+            ScenarioContext.StepIsPending();
+        }
+    }
+}
